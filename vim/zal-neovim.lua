c140de28dd7df94ba689f13ddc3116120c626fd9
zalasus <niklas.weissner@gmail.com>
2024-02-11 19:06:15 +0100

    Modernize (neo)vim config

---
+++ vim/zal-neovim.lua
@@ -0,0 +1,228 @@
+
+-- bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- ==== Plugin config ====
+
+local neotree = {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons",
+      "MunifTanjim/nui.nvim",
+    },
+    keys = {
+      { "<leader>t", "<cmd>Neotree toggle<cr>", desc = "NeoTree" },
+    },
+}
+
+local nvim_tree = {
+    'nvim-tree/nvim-tree.lua',
+    keys = {
+      { "<leader>t", "<cmd>NvimTreeToggle<cr>", desc = "NvimTree" },
+    },
+}
+
+local lualine = {
+    'nvim-lualine/lualine.nvim',
+    dependencies = 'nvim-tree/nvim-web-devicons'
+}
+
+local bufferline = {
+    'akinsho/bufferline.nvim',
+    version = "*",
+    dependencies = 'nvim-tree/nvim-web-devicons'
+}
+
+local telescope = {
+    'nvim-telescope/telescope.nvim',
+    tag = '0.1.5',
+    dependencies = { 'nvim-lua/plenary.nvim' }
+}
+
+require("lazy").setup({
+    "gentoo/gentoo-syntax",
+    "tpope/vim-fugitive",
+    "lervag/vimtex",
+    "ledger/vim-ledger",
+    "nvim-tree/nvim-web-devicons",
+    "neovim/nvim-lspconfig",
+    --"hrsh7th/nvim-cmp",
+    -- neotree,
+    nvim_tree,
+    lualine,
+    bufferline,
+    telescope,
+    "folke/tokyonight.nvim",
+})
+
+-- colorscheme
+
+require('tokyonight').setup({
+    style = 'storm',
+    --transparent = true,
+})
+vim.cmd[[colorscheme tokyonight]]
+
+-- require('neo-tree').setup()
+require('nvim-tree').setup()
+
+-- vimtex. gets very bitchy if this is not set.
+vim.g.tex_flavor = "latex"
+
+-- lualine
+local lualine = require('lualine')
+
+local lualine_diagnostics = {
+    'diagnostics',
+    sources = { 'nvim_lsp' },
+    sections = { 'error', 'warn', 'info', 'hint' },
+    diagnostics_color = {
+        -- Same values as the general color option can be used here.
+        error = 'DiagnosticError', -- Changes diagnostics' error color.
+        warn  = 'DiagnosticWarn',  -- Changes diagnostics' warn color.
+        info  = 'DiagnosticInfo',  -- Changes diagnostics' info color.
+        hint  = 'DiagnosticHint',  -- Changes diagnostics' hint color.
+    },
+    symbols = {error = 'E', warn = 'W', info = 'I', hint = 'H'},
+    colored = true,           -- Displays diagnostics status in color if set to true.
+    update_in_insert = false, -- Update diagnostics in insert mode.
+    always_visible = false,   -- Show diagnostics even if there are none.
+}
+
+local lualine_config = {
+    sections = {
+        lualine_a = { 'mode' }
+    },
+    theme = 'tokyonight',
+}
+
+lualine.setup(lualine_config)
+
+-- bufferline
+vim.opt.termguicolors = true
+local bufferline = require('bufferline')
+
+local bufferline_options = {
+    show_close_icon = false,
+    show_buffer_close_icons = false,
+    numbers = 'ordinal',
+    color_icons = true,
+    separator_style = 'slant',
+    always_show_bufferline = true,
+    diagnostics = 'nvim_lsp',
+    --themeable = true,
+    offsets = {
+        {
+            filetype = 'NvimTree',
+            text = 'NvimTree',
+            highlight = 'Directory',
+            separator = true,
+        }
+    },
+}
+
+local bufferline_highlights = {
+}
+
+bufferline.setup{ options = bufferline_options, highlights = bufferline_highlights }
+
+-- Telescope
+
+require('telescope').setup{
+}
+local telescope_builtin = require('telescope.builtin')
+local opts = { noremap = true, silent = true }
+vim.keymap.set('n', '<leader>ff',  telescope_builtin.find_files, opts)
+vim.keymap.set('n', '<leader>fg',  telescope_builtin.live_grep, opts)
+vim.keymap.set('n', '<leader>fs',  telescope_builtin.lsp_workspace_symbols, opts)
+
+-- ==== Line endings etc. ====
+
+-- autmatically strip trailing whitespace on save
+vim.api.nvim_create_autocmd({ "BufWritePre" }, {
+    pattern = {"*"},
+    callback = function(ev)
+        save_cursor = vim.fn.getpos(".")
+        vim.cmd([[%s/\s\+$//e]])
+        vim.fn.setpos(".", save_cursor)
+    end,
+})
+
+-- ==== Nvim options ====
+
+-- No mouse, thank you. Teaches me bad habits
+vim.opt.mouse = ''
+
+
+-- ==== LSP config =====
+
+-- Use an on_attach function to only map the following keys after the language server attaches to
+-- the current buffer
+local on_attach = function(client, bufnr)
+    -- Enable completion triggered by <c-x><c-o> (eugh)
+    vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+
+    -- Mappings.
+    -- See `:help vim.lsp.*` for documentation on any of the below functions
+    local bufopts = { noremap = true, silent = true, buffer = bufnr }
+
+    local telescope_builtin = require('telescope.builtin')
+    vim.keymap.set('n', 'gd', telescope_builtin.lsp_definitions, bufopts)
+    vim.keymap.set('n', 'gi', telescope_builtin.lsp_implementations, bufopts)
+    vim.keymap.set('n', 'gr', telescope_builtin.lsp_references, bufopts)
+
+    vim.keymap.set('n', '<leader>ch', vim.lsp.buf.hover, bufopts)
+    vim.keymap.set('n', '<leader>cr', vim.lsp.buf.rename, bufopts)
+    vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, bufopts)
+    vim.keymap.set('n', '<leader>cf', function() vim.lsp.buf.format { async = true } end, bufopts)
+end
+
+-- remap omnifunc to something sane
+vim.keymap.set('i', '<C-Space>', '<C-x><C-o>')
+vim.keymap.set('i', '<C-@>', '<C-Space>')
+
+local lsp_flags = {
+  debounce_text_changes = 300,
+}
+
+local lspconfig = require('lspconfig')
+
+-- rust-analyzer
+--
+-- rustup provides no proxy for rust-analyzer, we need to query it's path manually.
+function get_rust_analyzer_path()
+    local handle = io.popen('rustup which rust-analyzer')
+    local output = handle:read('*a')
+    local exit = handle:close()
+    local ra_path
+    if exit then
+        ra_path= vim.trim(output)
+    else
+        ra_path = "rust-analyzer"
+    end
+    return ra_path
+end
+
+lspconfig.rust_analyzer.setup {
+    on_attach = on_attach,
+    flags = lsp_flags,
+    cmd = { get_rust_analyzer_path() },
+    settings = {
+        ["rust-analyzer"] = {
+        }
+    }
+}
+
--- vim/zal.vim
+++ vim/zal.vim
@@ -1,22 +1,6 @@
-" Zal's nvim config :3
-
-" load plugins, using vimplug
-call plug#begin('~/.local/share/nvim/plugged')
-Plug 'gentoo/gentoo-syntax'
-Plug 'vim-airline/vim-airline'
-Plug 'vim-airline/vim-airline-themes'
-Plug 'rust-lang/rust.vim'
-Plug 'tpope/vim-fugitive'
-Plug 'mhinz/vim-startify'
-Plug 'lervag/vimtex'
-Plug 'ledger/vim-ledger'
-Plug 'nvim-tree/nvim-web-devicons'
-Plug 'nvim-tree/nvim-tree.lua'
-Plug 'neovim/nvim-lspconfig'
-call plug#end()
-
-syntax enable
-filetype plugin indent on
+" Zal's vim config
+"  Some basic stuff that should work on vim and nvim is done here in vimscript. All the advanced,
+"  productive things that requires a recent nvim and plugins happen in the lua config invoked below.
 
 " don't care about vi compatibility
 set nocompatible
@@ -38,13 +22,6 @@ set nowrap
 set tw=100
 set colorcolumn=+1
 
-" enable mouse-terminal interaction
-set mouse=a
-
-" key timeout
-set ttimeout
-set ttimeoutlen=50
-
 " always display a status line even if there is only one window
 set laststatus=2
 
@@ -54,13 +31,8 @@ set hidden
 " default spellcheck language
 set spelllang=en
 
-" don't create swap files. they tend to hang vim when the disk has high load,
-"  and my compulsive :w habit makes them superflous, anyway. also, when was the
-"  last time you saw vim crash?
-set noswapfile
-
 " use system clipboard transparently when yanking
-set clipboard=unnamed
+set clipboard=unnamed,unnamedplus
 
 " smart case search (only use case sensitive search if query contains uppercase)
 set ignorecase
@@ -78,12 +50,6 @@ nnoremap <leader>d :bd!<CR>
 nnoremap <leader>n :enew<CR>
 nnoremap <leader>q :q<CR>
 
-" faster compulsive saving
-nnoremap <leader>w :w<CR>
-
-" startify session restore
-autocmd! VimLeave * SSave! Previous
-
 " make arrow keys navigate splits rather than the cursor
 noremap <Up> <C-w>k
 noremap <Down> <C-w>j
@@ -98,46 +64,27 @@ inoremap <Right> <nop>
 " without yanking it
 vnoremap <leader>p "_dP
 
-" nvimtree stuff
-nnoremap <leader>t :NvimTreeToggle<CR>
-
-" terminal emulator stuff
-tnoremap <Esc> <C-\><C-n>
-
-" fugitive
-nnoremap <leader>G :G<CR>
-
 " don't need indentation with motions
 nnoremap < <<
 nnoremap > >>
 
-" vimtex
-let g:tex_flavor = 'latex'
-
-" ===== visual stuff =====
-" airline config
-let g:airline_powerline_fonts = 1
-let g:airline_theme = 'simple'
-let g:airline_highlighting_cache = 1
-let g:airline_extensions = ['branch', 'tabline']
-let g:airline#extensions#tabline#enabled = 1
-let g:airline#extensions#tabline#formatter = 'unique_tail_improved'
-let g:airline#extensions#tabline#fnamemod = ':t'
-
 " use proper box-drawing character for vertical splits
-set fillchars+=vert:┃
+set fillchars+=vert:│
 
-" ===== IDE features =====
+" enable syntax higlighting
+syntax enable
+
 " highlight matching parens by underlining them
 highlight MatchParen term=underline cterm=underline gui=underline ctermbg=NONE
-
-" highlight trailing whitespace
-highlight RedundantSpaces ctermbg=red guibg=red
-match RedundantSpaces /\s\+$/
 
 " always show signcolumn, but don't color it
 set signcolumn=yes
 highlight SignColumn ctermbg=none guibg=none
 
-" ===== Hand over to Lua config =====
-luafile $ZAL_DOTFILES/vim/zal-vim.lua
+" enable filetype detection, using type-specific plugins and indent files when available
+filetype plugin indent on
+
+if has('nvim')
+    luafile $ZAL_DOTFILES/vim/zal-neovim.lua
+endif
+
